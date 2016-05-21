5210c62aaf7dbe256ab3988d6c3406bbea1099d6
Xixiang Wu <xindefenbao1@126.com>
2016-05-21 20:39:53 +1000

    Update

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 @app.route("/home.html")
 def index():
     return render_template('home.html')
-    
+
 @app.route("/pivot_table.html")
 def pivot_table():
 
@@ -23,22 +23,22 @@ def pivot_table():
     c = conn.cursor()
 
     table = html_table(c)
-    
+
 #    for sublist in c.execute('SELECT * FROM USIODB'):
 #        print sublist
     c.close()
-    conn.close()    
-    
+    conn.close()
+
     return render_template('pivot_table.html',table = Markup(table))
 
 def html_table(c):
     # Generates table
     table = '<table class="table table-striped table-hover">'
-    
+
     header = 1
 
     for row in c.execute('SELECT * FROM USIODB'):
-        
+
         table += '<tr>'
         if header:
             table+='<thead>'
@@ -48,21 +48,22 @@ def html_table(c):
             header = 0
 
         else:
-        
+
             for data in row:
                 table+='<td>{}</td>'.format(data)
             table+='</tr>'
 
     table+='</table>'
-    return table    
+    return table
 
 @app.route("/pivot_table_builder.html")
 def pivot_table_builder():
+
     return render_template('pivot_table_builder.html')
+
+def table_selector():
+    return false
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=8765)
-    
-
-
-
